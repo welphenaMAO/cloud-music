72424b2b335449cdd94c28855ceb3d8a241145dd
welphenAmao <825878657@qq.com>
2022-01-23 22:35:24 +0800

    layout

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='app-main'>
+    <transition name="fade-transform" mode="out-in">
+      <keep-alive :include="cachedViews">
+        <div>
+          <router-view :key="key">
+            <div id="pageContent"></div>
+          </router-view>
+        </div>
+      </keep-alive>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from "vue-property-decorator";
+
+  @Component({})
+  export default class AppMain extends Vue{
+    get cachedViews() {
+      return this.$store.state.tagsVies.cachedViews
+    }
+    get key() {
+      return this.$route.fullPath
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='app-wrapper'>
+
+  </div>
+</template>
+
+<script>
+import { NavBar, TabBar, AppMain } from './components'
+export default {
+    name: '',
+
+    props: {
+
+    },
+    components: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    beforeMount() {
+
+    },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
